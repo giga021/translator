96f50768af23809f05b78703cb80a47e6de33cb9
Giga <Giga@DESKTOP-MA71J2V>
2019-03-30 17:22:14 +0100

    Search query sending with response rendering

---
+++ Translator/Controllers/SearchController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Translator.Controllers
+{
+	public class SearchController : ApiController
+	{
+		public async Task<HttpResponseMessage> Get(string query)
+		{
+			return await Task.FromResult(Request.CreateResponse(HttpStatusCode.OK, new
+			{
+				result = DateTime.Now.Ticks.ToString()
+			}));
+		}
+	}
+}
